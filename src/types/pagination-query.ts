53363c2543530ae438a802930eac2e471ed449fb
Kartikya Thakur <kartikyathakur@gmail.com>
2023-08-22 21:58:00 -0500

    Added PaginatedResponse

--- src/types/pagination-query.ts
+++ src/types/pagination-query.ts
@@ -1,5 +1,35 @@
-export type PaginationQuery = {
+import { PaginatedResponse } from "./paginated-response";
+
+export class PaginationQuery {
+    page: number;
     skip: number;
     limit: number;
     sort: string;
+
+    constructor(page: number, limit: number, sort: string) {
+        this.page = page;
+        this.skip = (page - 1) * limit;
+        this.limit = limit;
+        this.sort = sort;
+    }
+
+    [Symbol.iterator]() {
+        return [{
+            skip: this.skip,
+            limit: this.limit,
+            sort: this.sort
+        }][Symbol.iterator]();
+    }
+
+    generatePaginatedResponse<T>(total: number, data: T[]): PaginatedResponse<T> {
+        return {
+            data: data,
+            pagination: {
+                total: total,
+                count: data.length,
+                page: this.skip / this.limit + 1,
+                sort: this.sort
+            }
+        };
+    }
 };
--- src/utilities/pagination-request-helper.ts
+++ src/utilities/pagination-request-helper.ts
@@ -4,13 +4,9 @@ import { PaginationQuery } from "../types/pagination-query";
 export const getPaginationQuery: (context: ExecutionContext) => PaginationQuery = (context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
 
-    const skipValue = request.query.skip ? Number(request.query.skip) : 0;
+    const pageValue = request.query.page ? Number(request.query.page): 1;
     const limitValue = request.query.limit ? Number(request.query.limit) : 10;
-    const sortValue = request.query.sort ? request.query.sort : '-_id';        
+    const sortValue = request.query.sort ? request.query.sort : '-_id';
   
-    return {
-        skip: skipValue,
-        limit: limitValue,
-        sort: sortValue
-    };
+    return new PaginationQuery(pageValue, limitValue, sortValue);
 }
